161a4c680ec87465416165bb773860c0ba727fed
Yamil <issayamilrt@gmail.com>
2024-02-20 15:29:45 +0100

    remove unused import

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,5 @@
 import React, { useState } from 'react';
-import {BrowserRouter as Router, Routes, Route,Link} from "react-router-dom";
-
+import {Link} from "react-router-dom";
 import './App.css';
 import { useGetAllCharactersQuery,} from "./generated/graphql";
 
